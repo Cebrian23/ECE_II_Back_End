5cbae27063f23c4fc1f54f4c03166a6a0ae22e54
Miguel <miguelangel5cebrian@gmail.com>
2026-04-16 12:53:26 +0200

    ft. Utilities de las utilities del tipo Report

--- utilities/Users/utils_Reports.ts
+++ utilities/Users/utils_Reports.ts
@@ -1,7 +1,7 @@
 import { UsersCollection } from "../../db/conection.ts";
 import { Report_Short, ReportDB } from "../../types/Users/Report.ts";
 
-export const Transform_Medication = async (report: ReportDB): Promise<Response> => {
+export const Transform_Report = async (report: ReportDB): Promise<Response> => {
     const persona_exists = await UsersCollection.findOne({_id: report.patient});
         
     if(!persona_exists){
@@ -31,6 +31,5 @@ export const Short_Report = (report: ReportDB): Report_Short => {
         health_service_type: report.health_service_type,
         specialty: report.specialty,
         date: report.date,
-        data: report.data,
     }
 }
