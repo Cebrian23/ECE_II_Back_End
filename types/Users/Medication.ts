bfe9e4acc2b4b5817f4cff5d560bd29f7f3f31ae
Miguel <miguelangel5cebrian@gmail.com>
2026-04-19 03:20:27 +0200

    ft. Creación del tipo Med_info a partir de datos del tipo Medication

--- types/Users/Medication.ts
+++ types/Users/Medication.ts
@@ -6,13 +6,7 @@ export type MedicationDB = OptionalId<{
     name: string,
     patient: ObjectId,
     type: "Pastilla" | "Jarabe" | string,
-    info: {
-        doctor: ObjectId,
-        init_date: string,
-        days_duration: number,
-        amount_times_day?: number,
-        ml_time?: number,
-    }[],
+    info: Med_infoDB[],
 }>
 
 export type Medication = {
@@ -20,17 +14,27 @@ export type Medication = {
     name: string,
     patient: User_Short,
     type: "Pastilla" | "Jarabe" | string,
-    info: {
-        doctor: Doctor,
-        init_date: string,
-        days_duration: number,
-        amount_times_day?: number,
-        ml_time?: number,
-    }[],
+    info: Med_info[],
 }
 
 export type Medication_Short = {
     id: string,
     name: string,
     info: number
+}
+
+export type Med_infoDB = {
+   doctor: ObjectId,
+   init_date: string,
+   days_duration: number,
+   amount_times_day?: number,
+   ml_time?: number,
+}
+
+export type Med_info = {
+   doctor: Doctor,
+   init_date: string,
+   days_duration: number,
+   amount_times_day?: number,
+   ml_time?: number,
 }
